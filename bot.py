79e1fe5dabd00a6f90897aeafc96b58383afab25
Radik <rad.husainov2016@yandex.ru>
2022-02-28 21:36:40 +0400

    working bot

---
+++ bot.py
@@ -0,0 +1,30 @@
+from aiogram import Bot, types
+from aiogram.dispatcher import Dispatcher
+from aiogram.utils import executor
+import emoji
+
+from config import TOKEN
+
+
+bot = Bot(token=TOKEN)
+dp = Dispatcher(bot)
+
+
+@dp.message_handler(commands=['start'])
+async def process_start_command(message: types.Message):
+    await message.reply("Привет!\nНапиши мне что-нибудь!")
+
+
+@dp.message_handler(commands=['help'])
+async def process_help_command(message: types.Message):
+    await message.reply("Напиши мне что-нибудь, и я отпрпавлю этот текст тебе в ответ!")
+
+
+@dp.message_handler()
+async def echo_message(msg: types.Message):
+    await msg.reply(msg)
+    await msg.reply('hello :eyes:')
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp)
