fe2902ad46040f21723e9cc55897c6112d273613
Radik <rad.husainov2016@yandex.ru>
2022-08-07 20:24:14 +0400

    добавил кнопки

--- bot.py
+++ bot.py
@@ -1,7 +1,6 @@
 from aiogram import Bot, types
 from aiogram.dispatcher import Dispatcher
 from aiogram.utils import executor
-import emoji
 
 from config import TOKEN
 
@@ -10,9 +9,24 @@ bot = Bot(token=TOKEN)
 dp = Dispatcher(bot)
 
 
-@dp.message_handler(commands=['start'])
-async def process_start_command(message: types.Message):
-    await message.reply("Привет!\nНапиши мне что-нибудь!")
+@dp.message_handler(commands="start")
+async def cmd_start(message: types.Message):
+    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    button_1 = "Поиск торрентов"
+    button_2 = "Похуй"
+    keyboard.add(button_1, button_2)
+
+
+@dp.message_handler(commands="inline_url")
+async def cmd_inline_url(message: types.Message):
+    buttons = [
+        types.InlineKeyboardButton(text="GitHub", url="https://github.com"),
+        types.InlineKeyboardButton(
+            text="Оф. канал Telegram", url="tg://resolve?domain=telegram")
+    ]
+    keyboard = types.InlineKeyboardMarkup(row_width=2)
+    keyboard.add(*buttons)
+    await message.answer("Кнопки-ссылки", reply_markup=keyboard)
 
 
 @dp.message_handler(commands=['help'])
@@ -20,9 +34,9 @@ async def process_help_command(message: types.Message):
     await message.reply("Напиши мне что-нибудь, и я отпрпавлю этот текст тебе в ответ!")
 
 
-@dp.message_handler()
-async def echo_message(msg: types.Message):
-    await msg.reply(msg.text)
+@dp.message_handler(commands=['torrent'])
+async def process_torrent_command(message: types.Message):
+    await message.reply("Напиши мне что-нибудь, и я отпрпавлю этот текст тебе в ответ!")
 
 
 if __name__ == '__main__':
